d053cc62cb0912219fc3876df02fe675cdf6fc42
Narek <narek.dev13@gmail.com>
2025-05-23 15:20:51 +0400

    changes

--- app/Filament/Resources/LeadResource.php
+++ app/Filament/Resources/LeadResource.php
@@ -6,7 +6,8 @@ use App\Filament\Resources\LeadResource\Pages;
 use App\Filament\Resources\LeadResource\RelationManagers;
 use App\Models\{
     Data,
-    Status
+    Status,
+    Project
 };
 use Filament\Forms;
 use Filament\Forms\Form;
@@ -23,7 +24,8 @@ use Filament\Tables\Columns\{
     SelectColumn
 };
 use Filament\Tables\Filters\SelectFilter;
-
+use Filament\Tables\Filters\Filter;
+use Carbon\Carbon;
 class LeadResource extends Resource
 {
     use GetHelper;
@@ -109,7 +111,95 @@ class LeadResource extends Resource
                     {
                         $query->where('status_id', $data['value']);
                     }
+                }),
+                Filter::make('date_filter')
+                ->form([
+                    Forms\Components\Grid::make(2)
+                        ->schema([
+                            Forms\Components\Select::make('preset')
+                                ->label('Период')
+                                ->options([
+                                    'today' => 'Сегодня',
+                                    'yesterday' => 'Вчера',
+                                    'week' => 'Последняя неделя',
+                                    'month' => 'Последний месяц',
+                                ])
+                                ->columnSpan('full'),
+
+                            Forms\Components\DatePicker::make('from')
+                                ->label('От')
+                                ->columnSpan(1),
+
+                            Forms\Components\DatePicker::make('until')
+                                ->label('До')
+                                ->columnSpan(1),
+                        ]),
+                ])
+                ->query(function (Builder $query, array $data): Builder {
+                    if (!empty($data['preset'])) {
+                        switch ($data['preset']) {
+                            case 'today':
+                                return $query->whereDate('created_at', Carbon::today());
+                            case 'yesterday':
+                                return $query->whereDate('created_at', Carbon::yesterday());
+                            case 'week':
+                                return $query->whereDate('created_at', '>=', Carbon::now()->subDays(7));
+                            case 'month':
+                                return $query->whereDate('created_at', '>=', Carbon::now()->subMonth());
+                        }
+                    }
+
+                    return $query
+                        ->when($data['from'], fn ($q) => $q->whereDate('created_at', '>=', $data['from']))
+                        ->when($data['until'], fn ($q) => $q->whereDate('created_at', '<=', $data['until']));
                 })
+                ->indicateUsing(function (array $data): array {
+                    $indicators = [];
+
+                    if (!empty($data['preset'])) {
+                        $map = [
+                            'today' => 'Сегодня',
+                            'yesterday' => 'Вчера',
+                            'week' => 'Последняя неделя',
+                            'month' => 'Последний месяц',
+                        ];
+                        $indicators[] = $map[$data['preset']] ?? '';
+                    }
+
+                    if (!empty($data['from'])) {
+                        $indicators[] = 'От: ' . Carbon::parse($data['from'])->format('d.m.Y');
+                    }
+
+                    if (!empty($data['until'])) {
+                        $indicators[] = 'До: ' . Carbon::parse($data['until'])->format('d.m.Y');
+                    }
+
+                    return $indicators;
+                }),
+                SelectFilter::make('page')
+                ->label('Источник')
+                ->options(function () {
+                    return Data::where('user_id',auth()->user()->id)
+                    ->get()->pluck('page','page');
+                })
+                ->query(function ($query, $data) {
+                    if(isset($data['value']) && $data['value'])
+                    {
+                        $query->where('page', $data['value']);
+                    }
+                }),
+                SelectFilter::make('project')
+                ->label('Проекту')
+                ->options(function () {
+                    return Project::where('user_id',auth()->user()->id)
+                    ->get()->pluck('name','id');
+                })
+                ->query(function ($query, $data) {
+                    if(isset($data['value']) && $data['value'])
+                    {
+                        $query->where('project_id', $data['value']);
+                    }
+                }),
             ])
             ->actions([])
             ->bulkActions([])
