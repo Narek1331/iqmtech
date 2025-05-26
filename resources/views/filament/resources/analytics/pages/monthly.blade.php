b360b4b08232f94a71433fe30f2b8414f04a637a
Narek <narek.dev13@gmail.com>
2025-05-26 13:23:27 +0400

    chan

--- resources/views/filament/resources/analytics/pages/monthly.blade.php
+++ resources/views/filament/resources/analytics/pages/monthly.blade.php
@@ -1,34 +1,30 @@
 <x-filament::page>
-    <table class="min-w-full divide-y divide-gray-200">
-  <thead class="bg-gray-100">
-    <tr>
-      <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Месяц
-      </th>
-      <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-        Всего конт-ов
-      </th>
-      @foreach ($this->getStatuses() as $status)
-          <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 uppercase tracking-wider">
-            {{ $status->name }}
-        </th>
-      @endforeach
-    </tr>
-  </thead>
-  <tbody class="bg-white divide-y divide-gray-200">
-
-        @foreach ($this->getDatas() as $num => $datas)
+    <table class="min-w-full divide-y divide-gray-200 dark:divide-gray-700">
+        <thead class="bg-gray-100 dark:bg-gray-800">
             <tr>
-                @foreach ($datas as $data)
-                    <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900">
-                        {{ $data }}
-                    </td>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">
+                    Месяц
+                </th>
+                <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">
+                    Всего конт-ов
+                </th>
+                @foreach ($this->getStatuses() as $status)
+                    <th class="px-6 py-3 text-left text-xs font-medium text-gray-500 dark:text-gray-400 uppercase tracking-wider">
+                        {{ $status->name }}
+                    </th>
                 @endforeach
             </tr>
-        @endforeach
-
-  </tbody>
-</table>
-
+        </thead>
+        <tbody class="bg-white dark:bg-gray-900 divide-y divide-gray-200 dark:divide-gray-700">
+            @foreach ($this->getDatas() as $num => $datas)
+                <tr>
+                    @foreach ($datas as $data)
+                        <td class="px-6 py-4 whitespace-nowrap text-sm text-gray-900 dark:text-gray-100">
+                            {{ $data }}
+                        </td>
+                    @endforeach
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
 </x-filament::page>
-
